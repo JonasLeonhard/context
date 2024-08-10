e404f86535367d80757dff01c81fec587c74b378
jonas <jonasleonhardfl@gmail.com>
2024-08-10 21:57:46 +0200

    fix(ast): added toString helpers to statments & expressions

--- src/ast.zig
+++ src/ast.zig
@@ -24,6 +24,17 @@ pub const Program = struct {
             return "";
         }
     }
+
+    pub fn toString(self: Program) []const u8 {
+        const buffer = std.ArrayList(u8).init(self.allocator);
+        defer buffer.deinit();
+
+        for (self.statements) |statement| {
+            buffer.appendSlice(statement.toString());
+        }
+
+        return buffer.toOwnedSlice();
+    }
 };
 
 // _____________________________________________
@@ -31,10 +42,17 @@ pub const Program = struct {
 pub const Statement = union(enum) {
     declare_assign: DeclareAssignStatement,
     return_: ReturnStatement,
+    expression: ExpressionStatement,
 
     pub fn tokenLiteral(self: Statement) []const u8 {
         switch (self) {
             inline else => |case| return case.tokenLiteral(),
+        }
+    }
+
+    pub fn toString(self: Statement) []const u8 {
+        switch (self) {
+            inline else => |case| return case.toString(),
         }
     }
 };
@@ -47,11 +65,17 @@ pub const Expression = union(enum) {
             inline else => |case| return case.tokenLiteral(),
         }
     }
+
+    pub fn toString(self: Statement) []const u8 {
+        switch (self) {
+            inline else => |case| return case.toString(),
+        }
+    }
 };
 
 // _____________________________________________
 
-/// <ident> := <expr>; // something like foo := 5;
+/// <ident> := <expr>; // Example: foo := 5;
 pub const DeclareAssignStatement = struct {
     token: Token,
     ident_expr: IdentifierExpression,
@@ -59,28 +83,48 @@ pub const DeclareAssignStatement = struct {
     pub fn tokenLiteral(self: DeclareAssignStatement) []const u8 {
         return self.token.literal;
     }
+    pub fn toString(self: DeclareAssignStatement) []const u8 {
+        return self.ident_expr.tokenLiteral() + " " + self.token.tokenLiteral() + " " + self.expr.toString() + ";";
+    }
 };
 
-/// return <expression>; // something like return 5;
+/// return <expression>; // Example: return 5;
 pub const ReturnStatement = struct {
     token: Token,
     expr: ?Expression,
     pub fn tokenLiteral(self: ReturnStatement) []const u8 {
         return self.token.literal;
     }
+    pub fn toString(self: ReturnStatement) []const u8 {
+        return self.tokenLiteral() + " " + self.expr.toString() + ";";
+    }
 };
 
-/// <expression>; // something like x + 10;
-pub const ExpressionStatement = struct { value: ?Expression };
+/// <expression>; // Example: x + 10;
+pub const ExpressionStatement = struct {
+    /// the first token of the expression
+    token: Token,
+    expr: ?Expression,
+    pub fn tokenLiteral(self: ExpressionStatement) []const u8 {
+        return self.token.literal;
+    }
+    pub fn toString(self: ExpressionStatement) []const u8 {
+        return self.expr.toString() + ";";
+    }
+};
 
 // _____________________________________________
 
-/// <ident> // something like foo
+/// <ident> // Example foo
 pub const IdentifierExpression = struct {
     token: Token,
     ident: []const u8,
 
     pub fn tokenLiteral(self: IdentifierExpression) []const u8 {
         return self.token.literal;
+    }
+
+    pub fn toString(self: IdentifierExpression) []const u8 {
+        return self.ident;
     }
 };
