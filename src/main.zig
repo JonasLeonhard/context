f7115b81804cd7c042170082998e1d9d985a52a5
jonas <jonasleonhardfl@gmail.com>
2024-08-11 16:34:27 +0200

    fix(parser): expression parsing

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const token = @import("token.zig");
 const lexer = @import("lexer.zig");
+const parser = @import("parser.zig");
+// var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+// const alloc = gpa.allocator();
 
 pub fn main() !void {
-    // const lex = lexer.Lexer{};
-    // std.debug.print("All your {} are belong to us.\n", .{lex});
+    // TODO
 }
--- src/parser.zig
+++ src/parser.zig
@@ -7,6 +7,8 @@ const Program = ast.Program;
 const Statement = ast.Statement;
 const DeclareAssignStatement = ast.DeclareAssignStatement;
 const ReturnStatement = ast.ReturnStatement;
+const ExpressionStatement = ast.ExpressionStatement;
+const Expression = ast.Expression;
 const IdentifierExpression = ast.IdentifierExpression;
 const Lexer = @import("lexer.zig").Lexer;
 const Token = @import("token.zig").Token;
@@ -14,16 +16,26 @@ const TokenType = @import("token.zig").TokenType;
 const ArrayList = std.ArrayList;
 const test_allocator = std.testing.allocator;
 
+const prefix_parse_fn_map = std.StaticStringMap(TokenType).initComptime(.{
+    // TODO: TokenType to prefixParseFn
+    .{ "token", TokenType.Mut },
+});
+
+const infix_parse_fn_map = std.StaticStringMap(TokenType).initComptime(.{
+    // TODO: TokenType to infixParseFunction
+    .{ "mut", TokenType.Mut },
+});
+
+/// A recursive descent pratt parser
 pub const Parser = struct {
-    allocator: std.mem.Allocator,
     lexer: Lexer,
     prev_token: Token = Token{ .type = .Unknown, .literal = "" },
     cur_token: Token = Token{ .type = .Unknown, .literal = "" },
     peek_token: Token = Token{ .type = .Unknown, .literal = "" },
     errors: ArrayList([]const u8),
 
-    fn init(allocator: std.mem.Allocator, lex: Lexer) Parser {
-        var parser = Parser{ .allocator = allocator, .lexer = lex, .errors = ArrayList([]const u8).init(allocator) };
+    pub fn init(alloc: std.mem.Allocator, lex: Lexer) Parser {
+        var parser = Parser{ .lexer = lex, .errors = ArrayList([]const u8).init(alloc) };
 
         // read two tokens, so curToken and peekToken are both set
         parser.nextToken();
@@ -32,7 +44,7 @@ pub const Parser = struct {
         return parser;
     }
 
-    fn deinit(self: *Parser) void {
+    pub fn deinit(self: *Parser) void {
         self.errors.deinit();
     }
 
@@ -42,11 +54,11 @@ pub const Parser = struct {
         self.peek_token = self.lexer.nextToken();
     }
 
-    pub fn parseProgram(self: *Parser) !Program {
-        var program = Program.init(self.allocator);
+    pub fn parseProgram(self: *Parser, alloc: std.mem.Allocator) !Program {
+        var program = Program.init(alloc);
 
         while (self.cur_token.type != .Eof) {
-            const statement = try self.parseStatement();
+            const statement = try self.parseStatement(alloc);
             if (statement) |stmt| {
                 try program.statements.append(stmt);
             }
@@ -56,26 +68,26 @@ pub const Parser = struct {
         return program;
     }
 
-    fn parseStatement(self: *Parser) !?Statement {
+    fn parseStatement(self: *Parser, alloc: std.mem.Allocator) !?Statement {
         switch (self.cur_token.type) {
-            .DeclareAssign => return try self.parseDeclareAssignStatement(),
+            .DeclareAssign => return try self.parseDeclareAssignStatement(alloc),
             .Return => return try self.parseReturnStatement(),
             else => {
-                if (self.peek_token.type != .Unknown or self.peek_token.type != .Eof) {
+                if (self.peek_token.type != .Unknown and self.peek_token.type != .Eof) {
                     self.nextToken();
-                    return try self.parseStatement();
+                    return try self.parseStatement(alloc);
                 }
                 return null;
             },
         }
     }
 
-    fn parseDeclareAssignStatement(self: *Parser) !?Statement {
+    fn parseDeclareAssignStatement(self: *Parser, alloc: std.mem.Allocator) !?Statement {
         const declare_assign_token = self.cur_token;
         const ident_expr = IdentifierExpression{ .token = self.prev_token, .ident = self.prev_token.literal };
 
         if (!self.expectPrevAndEat(.Ident)) {
-            const err = try fmt.allocPrint(self.allocator, "Error parsing ':=' declare_assign. Expected identifier before declare_assign, but got: {s}\n", .{self.prev_token.literal});
+            const err = try fmt.allocPrint(alloc, "Error parsing ':=' declare_assign. Expected identifier before declare_assign, but got: {s}\n", .{self.prev_token.literal});
             try self.errors.append(err);
             return null;
         }
@@ -99,6 +111,31 @@ pub const Parser = struct {
         const statement = ReturnStatement{ .token = return_token, .expr = null }; // TODO: parse val
         return Statement{ .return_ = statement };
     }
+
+    // TODO
+    fn registerPrefix(self: Parser, token_type: TokenType, parse_func: fn () void) void {
+        _ = self;
+        _ = token_type;
+        _ = parse_func;
+    }
+
+    // TODO
+    fn registerInfix(self: Parser, token_type: TokenType, parse_func: fn () void) void {
+        _ = self;
+        _ = token_type;
+        _ = parse_func;
+    }
+
+    // fn prefixParseFn(self: Parser) Expression {
+    //     _ = self;
+    //     @compileError("TODO");
+    // }
+
+    // fn infixParseFn(self: Parser, left_expr: Expression) Expression {
+    //     _ = self;
+    //     _ = left_expr;
+    //     @compileError("TODO");
+    // }
 
     fn curTokenIs(self: Parser, token_type: TokenType) bool {
         return self.cur_token.type.compareEq(token_type);
@@ -129,7 +166,7 @@ pub const Parser = struct {
         return false;
     }
 
-    fn checkParserErrors(self: Parser) !void {
+    pub fn checkParserErrors(self: Parser) !void {
         if (self.errors.items.len == 0) {
             return;
         }
@@ -143,74 +180,102 @@ pub const Parser = struct {
     }
 };
 
-fn validDeclareAssignStatement(statement: Statement, ident: []const u8) !bool {
-    switch (statement) {
-        .declare_assign => |declareAssign| {
-            try testing.expectEqualStrings(declareAssign.tokenLiteral(), ":=");
-            try testing.expectEqualStrings(declareAssign.ident_expr.ident, ident);
-            try testing.expectEqualStrings(declareAssign.ident_expr.tokenLiteral(), ident);
-            return true;
-        },
-        else => return false,
-    }
-}
-
-fn validReturnStatement(statement: Statement) !bool {
-    switch (statement) {
-        .return_ => {
-            try testing.expectEqualStrings(statement.tokenLiteral(), "return");
-            return true;
-        },
-        else => return false,
-    }
-}
-
-test "DeclareAssign statements" {
+test "DeclareAssign Statement" {
     const input =
         \\x := 5;
-        \\y := 10;
-        \\foobar := 838383;
     ;
 
     const lexer = Lexer.init(input);
     var parser = Parser.init(test_allocator, lexer);
     defer parser.deinit();
 
-    var program = try parser.parseProgram();
+    var program = try parser.parseProgram(test_allocator);
     defer program.deinit();
     try parser.checkParserErrors();
 
-    try testing.expectEqual(program.statements.items.len, 3);
+    try testing.expectEqual(1, program.statements.items.len);
 
-    // TODO: test expressions here aswell
-    const tests = .{ .{ .expected_identifier = "x" }, .{ .expected_identifier = "y" }, .{ .expected_identifier = "foobar" } };
+    const tests = [_]Statement{
+        Statement{
+            // x := 5;
+            .declare_assign = DeclareAssignStatement{
+                .ident_expr = IdentifierExpression{ .ident = "x", .token = Token{ .type = .Ident, .literal = "x" } },
+                .token = Token{ .type = .DeclareAssign, .literal = ":=" },
+                .expr = null, // TODO
+            },
+        },
+    };
 
-    inline for (0.., tests) |i, test_item| {
-        const statement = program.statements.items[i];
-        const is_valid = try validDeclareAssignStatement(statement, test_item.expected_identifier);
-        try testing.expect(is_valid);
+    for (0.., tests) |i, expected| {
+        const actual = program.statements.items[i];
+        try testing.expectEqual(expected.declare_assign.token.type, actual.declare_assign.token.type);
+        try testing.expectEqualStrings(expected.declare_assign.token.literal, actual.declare_assign.token.literal);
+        try testing.expectEqualStrings(expected.declare_assign.ident_expr.ident, actual.declare_assign.ident_expr.ident);
+        try testing.expectEqual(expected.declare_assign.ident_expr.token.type, actual.declare_assign.ident_expr.token.type);
+        try testing.expectEqualStrings(expected.declare_assign.ident_expr.token.literal, actual.declare_assign.ident_expr.token.literal);
     }
 }
 
-test "Return statements" {
+test "Return Statement" {
     const input =
         \\return 5;
-        \\return 9000;
     ;
 
     const lexer = Lexer.init(input);
     var parser = Parser.init(test_allocator, lexer);
     defer parser.deinit();
 
-    var program = try parser.parseProgram();
+    var program = try parser.parseProgram(test_allocator);
     defer program.deinit();
     try parser.checkParserErrors();
 
-    try testing.expectEqual(program.statements.items.len, 2);
+    try testing.expectEqual(1, program.statements.items.len);
 
-    // TODO: test expressions here aswell
-    for (program.statements.items) |statement| {
-        const is_valid = try validReturnStatement(statement);
-        try testing.expect(is_valid);
+    const tests = [_]Statement{
+        // return 5;
+        Statement{
+            .return_ = ReturnStatement{
+                .token = Token{ .type = .Return, .literal = "return" },
+                .expr = null, // TODO
+            },
+        },
+    };
+
+    for (0.., tests) |i, expected| {
+        const actual = program.statements.items[i];
+        try testing.expectEqual(expected.return_.token.type, actual.return_.token.type);
+        try testing.expectEqualStrings(expected.return_.token.literal, actual.return_.token.literal);
+        try testing.expectEqual(expected.return_.expr, actual.return_.expr);
+    }
+}
+
+test "Expression Statement" {
+    const input = "foobar;";
+
+    const lexer = Lexer.init(input);
+    var parser = Parser.init(test_allocator, lexer);
+    defer parser.deinit();
+
+    var program = try parser.parseProgram(test_allocator);
+    defer program.deinit();
+    try parser.checkParserErrors();
+
+    try testing.expectEqual(1, program.statements.items.len);
+
+    const tests = [_]Statement{
+        // foobar;
+        Statement{
+            .expression = ExpressionStatement{
+                .token = Token{ .type = .Ident, .literal = "foobar" },
+                .expr = null, // TODO
+            },
+        },
+    };
+
+    for (0.., tests) |i, expected| {
+        const actual = program.statements.items[i];
+        try testing.expectEqual(expected.expression.token.type, actual.expression.token.type);
+        try testing.expectEqualStrings(expected.expression.token.literal, actual.expression.token.literal);
+        try testing.expectEqual(expected.expression.expr, actual.expression.expr);
     }
 }
